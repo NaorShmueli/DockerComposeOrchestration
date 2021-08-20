ea937aca776f753bffbbe01bd3e85296ebb8754c
naor2 <naor2@DESKTOP-2DV3RHL>
2021-08-20 18:21:35 +0300

    init

---
+++ ServiceA/ServiceC/Startup.Healthcheck.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceC
+{
+    public partial class Startup
+    {
+        public void AddHealthChecks(IServiceCollection services)
+        {
+        }
+    }
+}
